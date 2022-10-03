34906b95bdb83eec492305b0b3ede06137c26705
omkarg01 <omkargujja01@gmail.com>
2022-10-03 18:54:21 +0530

    most freq updated

--- ASSIGNMENTS/6/K-MostFreqWord.js
+++ ASSIGNMENTS/6/K-MostFreqWord.js
@@ -1,38 +1,37 @@
-import { MinHeap } from "datastructures-js";
+import { PriorityQueue } from "datastructures-js";
 
-const words = ["priya", "bhatia", "akshay", "arpit", "priya", "arpit"];
+function kFreq(words, k) {
+    const wordsMap = new Map();
 
-const wordsMap = new Map();
+    // counting freq of each word and storing it in map
+    words.forEach((eachword) => {
+        if (wordsMap.has(eachword)) {
+            wordsMap.set(eachword, wordsMap.get(eachword) + 1);
+        } else {
+            wordsMap.set(eachword, 1);
+        }
+    });
 
-words.forEach((eachword) => {
-    if (wordsMap.has(eachword)) {
-        wordsMap.set(eachword, wordsMap.get(eachword) + 1);
-    } else {
-        wordsMap.set(eachword, 1);
+    // converting map to arrays eg - { priya : 2, arpit : 1}  will be covert to - [[priya, 2], [arpit, 1]]
+    // this converion has been done for the ease of comparision
+    const mapEntries = [...wordsMap.entries()];
+
+    // storing entire array in minHeap : word with lowest freq will appeared at the top
+    const minHeap = PriorityQueue.fromArray(mapEntries, (a, b) => a[1] - b[1]);
+
+    // pop all words from the heap until the size of minheap is greater than k
+    while (minHeap.size() > k) {
+        minHeap.pop();
     }
-})
 
-const compareFreq = (a, b) => {
-    if (a[1] > b[1]) {
-        return -1;
+    // clear the last k elements in minHeap which will have the max frequency and store it ini res arr
+    const res = [];
+    while (!minHeap.isEmpty()) {
+        res.push(minHeap.pop()[0]);
     }
-    if (a[1] < b[1]) {
-        // prioratize newest cars
-        return 1;
-    }
-    // with least price
-    return a[1] < b[1] ? -1 : 1;
-};
-
-const minHeap = new MinHeap(compareFreq);
-const mapEntries = [...wordsMap.entries()]
-mapEntries.forEach((eachword) => {
-    minHeap.push(eachword)
-})
-
-
-while (!minHeap.size() > 3) {
-    minHeap.pop()
+    console.log(res)
 }
 
-console.log(minHeap)
+const words = ["priya", "bhatia", "akshay", "arpit", "priya", "arpit"];
+const k = 3;
+kFreq(words, k);
