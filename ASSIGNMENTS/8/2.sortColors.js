810785c2abfb833eeb730eb2bb63e967d813f9a8
omkarg01 <omkargujja01@gmail.com>
2022-10-12 16:35:28 +0530

    sortColors done

---
+++ ASSIGNMENTS/8/2.sortColors.js
@@ -0,0 +1,34 @@
+function sortColor(arr, p, q) {
+    if (p < q) {
+        const mid = sort(arr, p, q);
+        sortColor(arr, p, mid - 1);
+        sortColor(arr, mid + 1, q);
+    }
+}
+
+function sort(arr, p, q) {
+    const pivot = arr[p];
+    let i = p;
+    for (let j = i + 1; j <= q; j++) {
+        if (arr[j] < pivot) {
+            i += 1;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i, p);
+    return i;
+}
+
+function swap(arr, i, j) {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
+const arr = [2, 0, 2, 1, 1, 0];
+const p = 0;
+const q = arr.length - 1;
+console.log(arr)
+sortColor(arr, p, q)
+console.log(arr)
