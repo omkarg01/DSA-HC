8c392cbbd0a44190fdc164b14c6637aa2a5d99a7
omkarg01 <omkargujja01@gmail.com>
2022-10-12 17:16:52 +0530

    majority done

---
+++ ASSIGNMENTS/8/4.majorityElem.js
@@ -0,0 +1,13 @@
+function majority(nums) {
+    const map = arr.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+
+    const sortMap = new Map([...map].sort().reverse());
+
+    const highestPair = [...sortMap.entries()][0];
+
+    return highestPair[0];
+}
+
+const arr = [2, 2, 1, 1, 1, 2, 2];
+const res = majority(arr);
+console.log(res)
