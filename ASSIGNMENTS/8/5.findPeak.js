cffe0f641b78f1a298da44219d9f556b046c62dc
omkarg01 <omkargujja01@gmail.com>
2022-10-14 21:41:08 +0530

    comment

--- ASSIGNMENTS/8/5.findPeak.js
+++ ASSIGNMENTS/8/5.findPeak.js
@@ -1,36 +1,53 @@
 function binarySearch(nums, p, q) {
-    if (p <= q) {
+    if (p <= q) { // left pointer crosses the right pointer call will stop
         const mid = Math.floor(p + (q - p) / 2);
 
+        // mid is other than the first and last element of an array
         if (mid > 0 && mid < nums.length - 1) {
+            // if the mid element is greater than both of its neighbours [1, 3, 5, 10, 7, 6, 4]
             if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            } else if (nums[mid] < nums[mid + 1]) {
+            }
+            // if the mid element is smaller than its left neighbour (we are at the left side of the peak) [1, 3, 5, 7, 10, 4, 2]
+            else if (nums[mid] < nums[mid + 1]) {
                 return binarySearch(nums, mid + 1, q);
-            } else if (nums[mid] < nums[mid - 1]) {
+            }
+            // if the mid element is smaller than its right neighbour (we are at the right side of the peak) [1, 3, 10, 9, 8, 4, 2]
+            else if (nums[mid] < nums[mid - 1]) {
                 return binarySearch(nums, p, mid - 1);
             }
-        } else if (mid === 0) {
+        }
+        // mid first element of an array
+        else if (mid === 0) {
+            // if the first element is greater than its right neighbour [10, 8, 4, 2, 1]
             if (nums[0] > nums[mid + 1]) {
                 return 0;
-            } else {
+            }
+            // if the first element is smaller than its right neighbour [1, 10, 8, 4, 2]
+            else {
                 return 1;
             }
-        } else if (mid === nums.length - 1) {
+        }
+        // mid is last element of an array 
+        else if (mid === nums.length - 1) {
+            // if the last element is greater than its left neighbour [1, 2, 4, 8, 10]
             if (nums[nums.length - 1] > nums[nums.length - 2]) {
                 return nums.length - 1;
-            } else {
+            }
+            // if the last element is smaller than its left neighbour [1, 2, 4, 8, 10, 4]
+            else {
                 return nums.length - 2;
             }
         }
     }
 }
 
-const arr = [6, 5, 4, 3, 2, 3, 2];
-const p = 0;
-const q = arr.length - 1;
-const peak = binarySearch(arr, p, q);
-console.log(peak)
+const arr = [1, 3, 5, 10, 7, 6, 4];
+const p = 0; // start index
+const q = arr.length - 1; // end index
+
+const peakIndex = binarySearch(arr, p, q);
+console.log(peakIndex)
 
 // Time Complexity : O(logn)
 // Space Compelxity : O(logn) (height of tree)
